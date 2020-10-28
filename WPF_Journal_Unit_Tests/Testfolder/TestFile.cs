bcb7625750e70067adf83cf498be217c3509cf96
Michael Distler <Michael.Distler@web.de>
2020-10-28 19:39:26 +0100

    Integrated NUnit project

---
+++ WPF_Journal_Unit_Tests/Testfolder/TestFile.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using NUnit.Framework;
+using WPF_Login;
+
+namespace WPF_Journal_Unit_Tests.Testfolder
+{
+    [TestFixture]
+    public class TestFile
+    {
+        [Test]
+        public void MethodName_ScenarioDescription()
+        {
+            // Arrange
+            int x = 6;
+            int y = 1;
+            int expectedValue = 7;
+            MethodsForUnitTests test = new MethodsForUnitTests();
+
+            // Act
+            int result = test.SumSomething(x, y);
+
+            // Assert
+            Assert.AreEqual(expectedValue, result);
+
+        }
+    }
+}
---
+++ WPF_Login/MethodsForUnitTests.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPF_Login
+{
+    public class MethodsForUnitTests
+    {
+        public int SumSomething(int a, int b)
+        {
+            return a + b;
+        }        
+    }
+}
